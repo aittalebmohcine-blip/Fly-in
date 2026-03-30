573633e478ac1da796668c9ac5a999dc36512700
Mohcine Ait Taleb <mait-tal@e2r3p12.1337.ma>
2026-03-30 14:55:18 +0100

    parse hubs: coords done.

--- tools/parser.py
+++ tools/parser.py
@@ -1,12 +1,31 @@
 from pydantic import BaseModel
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Tuple
 
 
-def pars_int(value: str, key: str) -> int:
+def parse_int(value: str, key: str) -> int:
     try:
         return int(value)
     except ValueError:
         raise ValueError(f"{key} must be an integer (got '{value}')")
+
+
+def parse_coords(value: Tuple[str, str], key: str) -> Tuple[int, int]:
+    x: int = parse_int(value[0], key)
+    y: int = parse_int(value[1], key)
+    return (x, y)
+
+
+def parse_meta_data():
+    pass
+
+
+def parse_hub(value: str, key: str) -> Dict[str, Any]:
+    parts: List[str] = value.split(3)
+    return {
+        "name": parts[0],
+        "coords": parse_coords((parts[1], parts[2]), key),
+        "meta-data": parse_meta_data(parts[3])
+    }
 
 
 class DronesConfig(BaseModel):
@@ -24,43 +43,59 @@ META_DATA = {"ZONE", "COLOR", "MAX_DRONES", "MAX_LINK_CAPACITY"}
 def parsing_config_file(file_path: str) -> DronesConfig:
     errors: List[str] = []
     raw: Dict[str, Any] = {}
+
     try:
         with open(file_path, "r") as file:
+
             lineno: int
             line: str
             first_valid_line: bool = True
+
             for lineno, line in enumerate(file, 1):
-                # ---------------
+                # skip coments and impty lines
                 line = line.strip()
 
                 if not line or line.startswith("#"):
                     continue
 
+                # invalid line
                 if ":" not in line:
                     errors.append(f"Line {lineno}: missing ':'")
                     continue
 
+                # split
                 key: str
                 value: str
                 key, value = map(str.strip, line.split(":", 1))
                 key = key.upper()
 
+                # invalid key
                 if key not in ALLOWED_KEYS:
                     errors.append(f"Line {lineno}: unknown key '{key}'")
                     continue
 
+                # duplicated key
                 if key in raw:
                     errors.append(f"Line {lineno}: duplicate key '{key}'")
                     continue
-                # ---------------
+
                 try:
+                    # first line is nb_drones
                     if first_valid_line:
                         if key != "NB_DRONES":
                             raise ValueError(
                                 f"Line {lineno}: 1st line must be 'NB_DRONES'"
                             )
-                        raw[key] = pars_int(value, key)
+                        raw[key] = parse_int(value, key)
                         first_valid_line = False
+
+                    # parse hubs
+                    if key in {"START_HUB", "HUB", "END_HUB"}:
+                        result: Dict[str, Any] = parse_hub(value, key)
+                        raw[]
+
+                    # parse connections
+
                 except ValueError as e:
                     errors.append(f"Line {lineno}: {e}")
 
