c7b5e4a85e252039049fa0f5b060594df7fcbf8d
Mohcine Ait Taleb <mait-tal@e2r10p15.1337.ma>
2026-04-01 09:15:26 +0100

    add Network and Simulation classes and add docstrings

--- tools/parsing2.py
+++ tools/parsing2.py
@@ -23,7 +23,11 @@ class ZoneType(Enum):
 
 
 class Drone():
-    '''It decides when to move based on its internal state.'''
+    '''Represents a single drone.
+    Responsibility:
+    Track its own ID, current location,
+    status (moving, waiting, delivered), and path.
+    It decides when to move based on its internal state.'''
 
     def __init__(
             self,
@@ -39,6 +43,14 @@ class Drone():
 
 
 class Zone():
+    '''Represents a physical location (Hub, Start, End, Restricted, etc.).
+    Responsibility:
+    Know its coordinates,
+    type (normal/blocked/restricted/priority),
+    capacity limits (max_drones),
+    and which drones are currently inside it.
+    It validates if a drone can enter.'''
+
     def __init__(
             self,
             coords: Point,
@@ -53,7 +65,30 @@ class Zone():
 
 
 class Connection():
+    '''Represents the link between two zones.
+    Responsibility:
+    Know the two zones it connects,
+    its max_link_capacity,and track drones currently
+    traversing it (crucial for the 2-turn restricted movement rule).'''
+
     def __init__(self) -> None:
         self.connecete: Tuple[Zone, Zone]
         self.max_link_capacity: int
         self.currently_traversing: List[Drone]
+
+
+class Network():
+    '''The container for all Zones and Connections.
+    Responsibility:
+    Parse the input file, build the graph,
+    and provide methods to find neighbors or calculate path costs.'''
+    pass
+
+
+class Simulation():
+    '''The conductor of the system.
+    Responsibility:
+    Manage the "Turn" loop.
+    It asks every Drone what it wants to do,
+    asks every Zone/Connection if that action is allowed,
+    updates the state, and prints the output.'''
